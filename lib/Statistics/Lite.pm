c4e3e2fac945783c5ef7e1d0950725751ebd1cd8
Andreas Faafeng <andreas@faafeng.com>
2009-04-24 09:50:39 +0200

    Added sub frequencies. Fixed @_ -> @numbers in sub range.

--- lib/Statistics/Lite.pm
+++ lib/Statistics/Lite.pm
@@ -21,8 +21,7 @@ sub max (*@numbers)
 
 sub range (*@numbers)
 {
-    return unless @_;
-	return 1; ####################### !!!
+    return unless @numbers;
     return max(@numbers) - min (@numbers);
 }
 
@@ -89,6 +88,15 @@ sub stddevp (*@numbers)
     return sqrt variancep @numbers;
 }
 
+sub frequencies (*@numbers)
+{
+  return unless @numbers;
+  return ( @numbers[0], 1 ) unless @numbers.elems > 1;
+  my %count;
+  for @numbers { %count{$_}++; }
+  return %count;
+}
+
 sub statshash (*@numbers) {
   my %hash = (
     count     => @numbers.elems,
@@ -108,72 +116,3 @@ sub statshash (*@numbers) {
 }
 
 
-
-# sub statsinfo
-# {
-#   my %stats= statshash(@numbers);
-#   return <<".";
-# min       = $stats{min}
-# max       = $stats{max}
-# range     = $stats{range}
-# sum       = $stats{sum}
-# count     = $stats{count}
-# mean      = $stats{mean}
-# median    = $stats{median}
-# mode      = $stats{mode}
-# variance  = $stats{variance}
-# stddev    = $stats{stddev}
-# variancep = $stats{variancep}
-# stddevp   = $stats{stddevp}
-# .
-# }
-
-# sub frequencies
-# {
-#   return unless @numbers;
-#   return ( $_[0], 1 ) unless @numbers > 1;
-#   my %count;
-#   foreach(@numbers) { $count{$_}++; }
-#   return %count;
-# }
-
-
-# is(min(1,2,3),    1, "call min - functional interface");
-# is(max(1,2,3),    3, "call max - functional interface");
-# is(range(1,2,3),  2, "call range - functional interface");
-# is(sum(1,2,3),    6, "call sum - functional interface");
-# is(count(1,2,3),  3, "call count - functional interface");
-# is(mean(1,2,3),   2, "call mean - functional interface");
-# is(median(1,2,3), 2, "call median - functional interface");
-# is(mode(1,2,3),   2, "call mode - functional interface");
-
-# is(variance(1,2,3), 1, "call variance - functional interface");
-# is(stddev(1,2,3),   1, "call stddev - functional interface");
-
-# is(variancep(2,4,2,4), 1, "call variancep - functional interface");
-# is(stddevp(2,4,2,4),   1, "call stddevp - functional interface");
-
-
-# my %stats= statshash(1,2,3);
-
-# is($stats{min},    1, "call min - hash-based interface");
-# is($stats{max},    3, "call max - hash-based interface");
-# is($stats{range},  2, "call range - hash-based interface");
-# is($stats{sum},    6, "call sum - hash-based interface");
-# is($stats{count},  3, "call count - hash-based interface");
-# is($stats{mean},   2, "call mean - hash-based interface");
-# is($stats{median}, 2, "call median - hash-based interface");
-# is($stats{mode},   2, "call mode - hash-based interface");
-
-# is($stats{variance}, 1, "call variance - hash-based interface");
-# is($stats{stddev},   1, "call stddev - hash-based interface");
-
-
-# %stats= statshash(2,4,2,4);
-# ok($stats{variancep}, "call variancep - hash-based interface");
-# ok($stats{stddevp},   "call stddevp - hash-based interface");
-
-# %stats= frequencies(1,2,3,3);
-# is($stats{1},1, "frequencies matched correctly");
-# is($stats{2},1, "frequencies matched correctly");
-# is($stats{3},2, "frequencies matched correctly");
